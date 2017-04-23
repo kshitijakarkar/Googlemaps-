e4102004ba801985eef0f18116781addd08a03ec
kshitija <kkarkar@mail.csuchico.edu>
2017-04-22 20:25:00 -0700

    Connected with firebase and display location from database

--- app/src/main/java/com/example/kshitija/googlemaps/MapsActivity.java
+++ app/src/main/java/com/example/kshitija/googlemaps/MapsActivity.java
@@ -10,8 +10,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
@@ -21,7 +24,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("markers");
-
+    DatabaseReference mchild = myRef.child("marker1");
 
 
     @Override
@@ -32,6 +35,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //myRef.setValue("Hello, World!");
     }
 
 
@@ -49,12 +54,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
        // myRef.setValue("Hello, World!");
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(18.520007, 73.851282);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Pune"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // double lo = 18.52;
+        //double la = 73.85;
+        //LatLng sydney = new LatLng(lo, la);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Pune"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //final double lng=0;
+        //double lat=0;
 
+        mchild.child("lat").setValue(73.85);
+        mchild.child("lon").setValue(18.52);
+        mchild.child("title").setValue("Pune");
+        mchild.child("access").setValue(1);
 
+        mchild.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Double lng = dataSnapshot.child("lon").getValue(Double.class);
+                Double lat = dataSnapshot.child("lat").getValue(Double.class);
+                String loc = dataSnapshot.child("title").getValue(String.class);
+                //System.out.println("Lon : .............."+lng);
+                //System.out.println("lat : .............."+lat);
 
+                LatLng pune = new LatLng(lng,lat);
+                mMap.addMarker(new MarkerOptions().position(pune).title(loc));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(pune));
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //LatLng pune = new LatLng(lat,lng);
+        //mMap.addMarker(new MarkerOptions().position(pune).title("PUNE"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(pune));
     }
 }
