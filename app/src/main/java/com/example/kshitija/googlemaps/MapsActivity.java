f011a38a295a867492e8563cd186f82a5932717e
kshitija <kkarkar@mail.csuchico.edu>
2017-04-23 19:29:43 -0700

    Randomly display 10 locations on google map from 20 locations stored in Firebase database

--- app/src/main/java/com/example/kshitija/googlemaps/MapsActivity.java
+++ app/src/main/java/com/example/kshitija/googlemaps/MapsActivity.java
@@ -1,30 +1,66 @@
 package com.example.kshitija.googlemaps;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+import static android.R.attr.data;
+import static android.R.attr.value;
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import static android.media.CamcorderProfile.get;
+import static android.os.Build.VERSION_CODES.N;
+
+
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+   public static class StoreLocations{
+
+       public Double latitude,longitude;
+       public String name;
+       public Integer access, item;
+
+       public StoreLocations(){}
+
+       public StoreLocations(Double latitude,Double longitude, String name,Integer access,Integer item)
+       {
+           this.name = name;
+           this.latitude = latitude;
+           this.longitude = longitude;
+           this.item = item;
+           this.access = access;
+       }
+
+
+   }
+
+
     private GoogleMap mMap;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("markers");
-    DatabaseReference mchild = myRef.child("marker1");
+    //DatabaseReference mchild = myRef.child("marker2");
 
 
     @Override
@@ -52,43 +88,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-       // myRef.setValue("Hello, World!");
-        // Add a marker in Sydney and move the camera
-       // double lo = 18.52;
-        //double la = 73.85;
-        //LatLng sydney = new LatLng(lo, la);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Pune"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        //final double lng=0;
-        //double lat=0;
+        LatLngBounds California = new LatLngBounds(new LatLng(34.352,-122.20),new LatLng(35.09,-113.58));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(California.getCenter(),5));
 
-        mchild.child("lat").setValue(73.85);
-        mchild.child("lon").setValue(18.52);
-        mchild.child("title").setValue("Pune");
-        mchild.child("access").setValue(1);
-
-        mchild.addValueEventListener(new ValueEventListener() {
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Double lng = dataSnapshot.child("lon").getValue(Double.class);
-                Double lat = dataSnapshot.child("lat").getValue(Double.class);
-                String loc = dataSnapshot.child("title").getValue(String.class);
-                //System.out.println("Lon : .............."+lng);
-                //System.out.println("lat : .............."+lat);
 
-                LatLng pune = new LatLng(lng,lat);
-                mMap.addMarker(new MarkerOptions().position(pune).title(loc));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(pune));
+                ArrayList<StoreLocations> randomstore = new ArrayList<>();
+
+                for (DataSnapshot detailsSnapshot : dataSnapshot.getChildren()) {
+
+
+                    Double lng = detailsSnapshot.child("lon").getValue(Double.class);
+                    Double lat = detailsSnapshot.child("lat").getValue(Double.class);
+                    String loc = detailsSnapshot.child("title").getValue(String.class);
+                    Integer acc = detailsSnapshot.child("access").getValue(Integer.class);
+                    Integer item = detailsSnapshot.child("item").getValue(Integer.class);
+
+                    StoreLocations s = new StoreLocations(lat, lng, loc, acc, item);
+
+                    randomstore.add(s);
+
+                }
+                Collections.shuffle(randomstore);
+
+                for(int i =0;i<10;i++){
+                    StoreLocations s = randomstore.get(i);
+                    System.out.println("item : .............."+s.item);
+                    LatLng pos = new LatLng(s.longitude,s.latitude);
+                    mMap.addMarker(new MarkerOptions().position(pos).title(s.name));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+                }
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+                System.out.println("The read failed: " + databaseError.getCode());
+
             }
         });
 
-        //LatLng pune = new LatLng(lat,lng);
-        //mMap.addMarker(new MarkerOptions().position(pune).title("PUNE"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(pune));
     }
+
+
 }
